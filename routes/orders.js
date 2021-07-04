0d096b0ca2253f6aa2d96431fc4b98496d33651d
Ricardo Dias <42947771+rycardodias@users.noreply.github.com>
2021-07-04 22:02:37 +0100

    .

--- routes/orders.js
+++ routes/orders.js
@@ -8,95 +8,125 @@ const ResponseModel = require('../lib/ResponseModel')
 const { error_missing_fields, error_invalid_fields, error_data_not_found, success_row_delete, error_row_delete, success_row_update,
     error_row_update, error_row_create, success_row_create } = require('../lib/ResponseMessages')
 
-    router.get('/', async (req, res) => {
-        const response = new ResponseModel()
-        try {
-            const request = await Model.findAll()
-            if (request.length > 0) {
-                response.data = request
-                res.status(200).json(response)
-            } else {
-                response.error = error_data_not_found
-                res.status(404).json(response)
-            }
-        } catch (error) {
-            response.message = error_data_not_found
-            response.error = error
-            return res.status(400).json(response)
+router.get('/', async (req, res) => {
+    const response = new ResponseModel()
+    try {
+        const request = await Model.findAll()
+        if (request.length > 0) {
+            response.data = request
+            res.status(200).json(response)
+        } else {
+            response.error = error_data_not_found
+            res.status(404).json(response)
         }
-    
-    })
-    
-    router.get('/id/:id', async (req, res) => {
-        const response = new ResponseModel()
-        try {
-            if (!req.params.id) {
-                response.error = error_missing_fields
-                res.status(400).json(response)
-            }
-            const request = await Model.findByPk(req.params.id)
-    
-            if (request) {
-                response.data = request
-                res.status(200).json(response)
-            } else {
-                response.error = error_data_not_found
-                res.status(404).json(response)
-            }
-        } catch (error) {
-            response.message = error_invalid_fields
-            response.error = error
-            return res.status(400).json(response)
-        }
-    
-    })
-
-router.post('/create', (req, res) => {
-    const { UserId, total, totalVAT, adress, locale, zipcode, observation, fiscalNumber } = req.body
-
-    Model.create({
-        UserId: UserId,
-        total: total,
-        totalVAT: totalVAT,
-        adress: adress,
-        locale: locale,
-        zipcode: zipcode,
-        observation: observation,
-        fiscalNumber: fiscalNumber
-
-    })
-        .then(status => res.json({ data: status }))
-        .catch(err => res.send(err))
-})
-
-
-router.put('/update', (req, res) => {
-    const { id, UserId, total, totalVAT, adress, locale, zipcode, observation, fiscalNumber } = req.body
-
-    if (id == undefined || id == "") {
-        res.send("Error! An id must be provided!")
+    } catch (error) {
+        response.message = error_data_not_found
+        response.error = error
+        return res.status(400).json(response)
     }
 
-    const data = {
-        UserId: UserId,
-        total: total,
-        totalVAT: totalVAT,
-        adress: adress,
-        locale: locale,
-        zipcode: zipcode,
-        observation: observation,
-        fiscalNumber: fiscalNumber
+})
+
+router.get('/id/:id', async (req, res) => {
+    const response = new ResponseModel()
+    try {
+        if (!req.params.id) {
+            response.error = error_missing_fields
+            res.status(400).json(response)
+        }
+        const request = await Model.findByPk(req.params.id)
+
+        if (request) {
+            response.data = request
+            res.status(200).json(response)
+        } else {
+            response.error = error_data_not_found
+            res.status(404).json(response)
+        }
+    } catch (error) {
+        response.message = error_invalid_fields
+        response.error = error
+        return res.status(400).json(response)
     }
 
-    Model.update(data,
-        {
-            where: {
-                id: id
-            },
-        })
-        .then(status => res.json({ data: status }))
-        .catch(err => console.log(err))
 })
+
+router.post('/create', async (req, res) => {
+    const response = new ResponseModel()
+    try {
+        const { UserId, total, totalVAT, adress, locale, zipcode, observation, fiscalNumber } = req.body
+
+
+        if (!(UserId && total && totalVAT && adress && locale && zipcode && fiscalNumber)) {
+            response.error = error_missing_fields
+            return res.status(400).json(response)
+        }
+
+        const data = {
+            UserId: UserId,
+            total: total,
+            totalVAT: totalVAT,
+            adress: adress,
+            locale: locale,
+            zipcode: zipcode,
+            observation: observation,
+            fiscalNumber: fiscalNumber
+        }
+
+        const request = await Model.create(data)
+
+        if (request) {
+            response.message = success_row_create
+            response.data = request
+            res.status(200).json(response)
+        } else {
+            response.error = error_row_create
+            res.status(404).json(response)
+        }
+    } catch (error) {
+        response.message = error_invalid_fields
+        response.error = error
+        return res.status(400).json(response)
+    }
+})
+
+router.put('/update', async (req, res) => {
+    const response = new ResponseModel()
+    try {
+        const { id, UserId, total, totalVAT, adress, locale, zipcode, observation, fiscalNumber } = req.body
+
+        if (!id) {
+            response.error = error_missing_fields
+            res.status(400).json(response)
+        }
+
+        const data = {
+            UserId: UserId,
+            total: total,
+            totalVAT: totalVAT,
+            adress: adress,
+            locale: locale,
+            zipcode: zipcode,
+            observation: observation,
+            fiscalNumber: fiscalNumber
+        }
+
+        const request = await Model.update(data, { where: { id: id } })
+
+        if (request == 1) {
+            response.data = success_row_update
+            res.status(200).json(response)
+        } else {
+            response.error = error_row_update
+            res.status(404).json(response)
+        }
+    } catch (error) {
+        response.message = error_invalid_fields
+        response.error = error
+        return res.status(400).json(response)
+    }
+})
+
 
 router.delete('/delete', async (req, res) => {
     const response = new ResponseModel()
