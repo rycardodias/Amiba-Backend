92d49e6f3d2ec3ff96c81c2d8426635b5a395f1e
Ricardo Dias <42947771+rycardodias@users.noreply.github.com>
2021-08-18 14:33:49 +0100

    .

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = new Sequelize(process.env.DATABASE, process.env.USER, process.e
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
-    // ssl: {
-    //   require: true,
-    //   // rejectUnauthorized: false
-    // }
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   },
 });
