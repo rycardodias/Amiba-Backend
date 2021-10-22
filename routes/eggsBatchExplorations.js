003a2170e788cb7bbea182a7995981cb358e82e6
Ricardo Dias <rycardo.dias@hotmail.com>
2021-10-22 16:44:35 +0100

    .

--- routes/eggsBatchExplorations.js
+++ routes/eggsBatchExplorations.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const Model = require('../models/User')
+const Model = require('../models/EggsBatchExploration')
 // const cache = require('../lib/cache/routeCache')
 // const removeCache = require('../lib/cache/removeCache')
 // const ResponseModel = require('../lib/ResponseModel')
