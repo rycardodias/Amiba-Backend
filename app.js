3cdc6a55cc31933c7f677a4ec5c29e4c86c0ba9a
Ricardo Dias <rycardo.dias@hotmail.com>
2021-10-22 14:17:32 +0100

    .

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(
-  cors({
+  cors(
+    // {
   //   origin: 'http://localhost:3000/',
   //   optionsSuccessStatus: 200,
-  })
+  // }
+  )
 );
 
 app.use(
