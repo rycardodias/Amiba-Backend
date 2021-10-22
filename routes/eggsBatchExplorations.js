21194ed04c6019f2f50d01f04b6a9468d336aedd
Ricardo Dias <rycardo.dias@hotmail.com>
2021-10-22 16:47:33 +0100

    .

--- routes/eggsBatchExplorations.js
+++ routes/eggsBatchExplorations.js
@@ -3,8 +3,8 @@ const router = express.Router()
 
 const Model = require('../models/EggsBatchExploration')
 // const cache = require('../lib/cache/routeCache')
-// const removeCache = require('../lib/cache/removeCache')
-// const ResponseModel = require('../lib/ResponseModel')
+const removeCache = require('../lib/cache/removeCache')
+const ResponseModel = require('../lib/ResponseModel')
 const { error_missing_fields, error_invalid_fields, error_data_not_found, success_row_delete, error_row_delete, success_row_update,
     error_row_update, error_row_create, success_row_create } = require('../lib/ResponseMessages')
 const Exploration = require('../models/Exploration')
