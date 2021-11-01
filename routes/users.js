512c0d746f05241027f5fc44a93c81e1ccb33346
Ricardo Dias <rycardo.dias@hotmail.com>
2021-11-01 14:03:27 +0000

    .

--- routes/users.js
+++ routes/users.js
@@ -181,7 +181,10 @@ router.update('/update/password', async (req, res) => {
         }
 
         const idToken = jwt.verify(token, "MySecret").id
-
+        const res = await Model.findByPk(id)
+        console.log(res)
+        const matchPasswords = bcrypt.compareSync(oldPassword, res.id)
+        console.log(matchPasswords)
         if (idToken != id) {
             response.message = error_invalid_token
             response.error = error_invalid_token
