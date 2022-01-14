75b03dbbf25621a0d4dc0c2ba9408b70d1967aff
rycardodias <rycardo.dias@hotmail.com>
2022-01-14 15:51:27 +0000

    .

--- routes/carts.js
+++ routes/carts.js
@@ -75,7 +75,7 @@ router.get('/UserId', async (req, res) => {
         }
 
         let tokenDecoded = jwt.verify(req.session.token, process.env.TOKEN_SECRET)
-        return res.status(200).json(req.session.token)
+        return res.status(200).json(req.session.token, tokenDecoded)
 
         const request = await Model.findAll({ where: { UserId: tokenDecoded.id } })
 
