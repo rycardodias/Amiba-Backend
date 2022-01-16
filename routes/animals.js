7e47daf6d4463bff81a7942b50fc7d07d8e53d2a
rycardodias <rycardo.dias@hotmail.com>
2022-01-16 22:48:28 +0000

    .

--- routes/animals.js
+++ routes/animals.js
@@ -142,7 +142,7 @@ router.post('/create', async (req, res) => {
         }
     } catch (error) {
         response.message = error_invalid_fields
-        response.error = error.toString()
+        response.error = error
         return res.status(400).json(response)
     }
 })
