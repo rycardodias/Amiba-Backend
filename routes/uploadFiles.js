c7afb73695408b18ddf5ec47cd6d1a48d694a964
rycardodias <rycardo.dias@hotmail.com>
2022-01-20 14:41:52 +0000

    .

--- routes/uploadFiles.js
+++ routes/uploadFiles.js
@@ -34,14 +34,14 @@ router.post("/create", async (req, res) => {
     const response = new ResponseModel()
     try {
         const requiredSizes = ["1:1", "16:9"] // JSON.parse(req.body.requiredSizes)
-        // const file = req.files.file
-        // const fileName = file.name
-        // const size = file.data.length
-        // const extension = path.extname(fileName)
+        const file = req.files.file
+        const fileName = file.name
+        const size = file.data.length
+        const extension = path.extname(fileName)
 
         const allowedExtensions = /png|jpeg|jpg|gif/
 
-        return res.status(500).json({ requiredSizes: requiredSizes })
+        return res.status(200).json({ requiredSizes: requiredSizes, fileName: fileName + extension })
 
         if (!allowedExtensions.test(extension)) throw "Unsupported extension!"
         if (size > 1024 * 1024 * 5) throw "File must be less than 5MB"
