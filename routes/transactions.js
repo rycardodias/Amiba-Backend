f8017c1514653173550a5ba8d8f2c94e614759dc
Ricardo Dias <rycardo.dias@hotmail.com>
2022-02-28 19:24:51 +0000

    addnewLoteTransaction

--- routes/transactions.js
+++ routes/transactions.js
@@ -13,6 +13,8 @@ const { Op } = require("sequelize");
 const AnimalProduct = require('../models/AnimalProduct')
 const EggsBatchProduct = require('../models/EggsBatchProduct')
 const jwt = require("jsonwebtoken");
+const EggsBatch = require('../models/EggsBatch');
+const EggsBatchLine = require('../models/EggsBatchLine');
 
 const handleProductsQuantity = async (quantity, data) => {
     let i = quantity
@@ -116,88 +118,34 @@ router.post('/createOrderOrderLines', async (req, res) => {
     }
 })
 
-// router.post('/createOrderOrderLines', async (req, res) => {
-//     const response = new ResponseModel()
-//     try {
-//         const { address, locale, zipcode, observation, fiscalNumber, OrderLines } = req.body
+router.post('/createEggsBatchEggsBatchLines', async (req, res) => {
+    const response = new ResponseModel()
+    try {
+        const { name, ExplorationId, EggsBatchLines } = req.body
+        const { token } = req.session
 
-//         const { token } = req.session
+        let eggsBatchs
+        await db.transaction(async (t) => {
+            eggsBatchs = await EggsBatch.create({ name: name, ExplorationId: ExplorationId, validity: new Date() }, { transaction: t })
 
-//         if (!token && !process.env.DEV_MODE) {
-//             response.message = error_missing_fields
-//             response.error = error_missing_fields
-//             return res.status(400).json(response)
-//         }
+            for (const i of EggsBatchLines) {
+                await EggsBatchLine.create(
+                    {
+                        EggsBatchId: eggsBatchs.dataValues.id,
+                        quantity: i.quantity
+                    },
+                    { transaction: t })
+            }
+        })
 
-//         let tokenDecoded = jwt.verify(token || process.env.DEV_MODE_TOKEN, process.env.TOKEN_SECRET)
-
-//         const data = {
-//             UserId: tokenDecoded.id,
-//             address: address,
-//             locale: locale,
-//             zipcode: zipcode,
-//             observation: observation,
-//             fiscalNumber: fiscalNumber
-//         }
-
-//         const result = await db.transaction(async (t) => {
-
-//             const order = await Order.create(data, { transaction: t })
-
-//             let dataLines, total, totalVAT
-
-//             for (const element of OrderLines) {
-//                 element.AnimalProduct ? (
-//                     (element.AnimalProduct.Product.unit === "KG") ?
-//                         (total = element.quantity *
-//                             (element.AnimalProduct.Product.price * element.AnimalProduct.weight) / 1000)
-//                         :
-//                         (total = element.quantity * element.AnimalProduct.Product.price)
-//                 ) : (
-//                     total = element.quantity * element.EggsBatchProduct.Product.price /
-//                     (element.EggsBatchProduct.Product.unit === 'DOZEN' ? 12 : 6)
-//                 )
-
-//                 element.AnimalProduct ?
-//                     (totalVAT = total * element.AnimalProduct.Product.tax / 100) :
-//                     (totalVAT = total * element.EggsBatchProduct.Product.tax / 100)
-
-//                 dataLines = {
-//                     OrderId: order.dataValues.id,
-//                     quantity: element.quantity,
-//                     total: total,
-//                     totalVAT: totalVAT,
-//                     AnimalProductId: element.AnimalProductId !== null ? element.AnimalProductId : undefined,
-//                     EggsBatchProductId: element.EggsBatchProductId !== null ? element.EggsBatchProductId : undefined,
-//                 }
-
-//                 const res = await OrderLine.create(dataLines, { transaction: t })
-//                 console.log(res.dataValues.AnimalProductId !== null)
-//                 if (res.dataValues.AnimalProductId !== null) {
-//                     await Cart.destroy({
-//                         where: {
-//                             UserId: tokenDecoded.id, AnimalProductId: res.dataValues.AnimalProductId
-//                         }
-//                     }, { transaction: t })
-//                 } else {
-//                     await Cart.destroy({
-//                         where: {
-//                             UserId: tokenDecoded.id, EggsBatchProductId: res.dataValues.EggsBatchProductId
-//                         }
-//                     }, { transaction: t })
-//                 }
-//             }
-
-//             response.message = success_row_create
-//             response.data = order
-//             return res.status(200).json(response)
-
-//         });
-//     } catch (error) {
-//         response.message = error_data_not_found
-//         response.error = error
-//         return res.status(400).json(response)
-//     }
-// })
+        response.message = success_row_create
+        response.data = eggsBatchs
+        return res.status(201).json(response)
+    } catch (error) {
+        response.message = error_data_not_found
+        response.error = error
+        return res.status(400).json(response)
+    }
+})
 
 module.exports = router
